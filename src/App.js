6730f76fb528616e662357fe67238c63ec91d0a6
Dasol-Jung <xenosophian@gmail.com>
2018-05-04 10:41:42 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import Person from './Person/Person'
+import './Person/Person.css'
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <Welcome />
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-    );
-  }
+class App extends Component{
+
+  state = {
+    persons : [
+      {name : "Aiden", age: 29},
+      {name : "Celia", age : 31},
+      {name : "React16", age : 1}
+    ],
+    showPersons : false
 }
 
+deletePersonHandler = (personIndex)=>{
+
+  const persons = [...this.state.persons];
+  persons.splice(personIndex,1);
+  this.setState({persons : persons})
+
+}
+
+togglePersonsHandler = ()=>{
+  this.setState({
+  showPersons : !this.state.showPersons
+  })
+}
+
+render(){
+  const style={
+    backgroundColor : '#eee',
+    fontSize : '1.5rem',
+    fontFamily : "Arial",
+    padding : '30px',
+    border : '1px solid lightblue',
+    cursor : 'pointer'
+  };
+
+  //normal javascript works here
+
+  let persons =null;
+
+  if(this.state.showPersons){
+    persons = this.state.persons.map((person, index)=>{
+      return <Person name={person.name} age={person.age} click={()=>this.deletePersonHandler(index)}/>
+    })
+  }
+
+  return (
+
+  //inside return, JSX not javascript
+    <div className="App">
+    <h1>React App</h1>
+    <p>Hello</p>
+    <button style ={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+    {persons}
+    </div>
+  );
+}
+
+}
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Person.css'
+
+const person = (props)=>{
+
+       return (
+           <div className="Person">
+            <h3 onClick={props.click}>{props.name}'s age : {props.age}</h3>
+            <input type="text" onChange={props.changed} value={props.name}/>
+            </div>
+       )
+
+
+    }
+    
+
+export default person;
